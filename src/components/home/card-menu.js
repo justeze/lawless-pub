bc20c8a4e19ef98d00825d51059c8cb6268c0185
justeze <faiz.sulistyawan@gmail.com>
2020-08-08 15:26:18 +0700

    mentoglagi

---
+++ src/components/home/card-menu.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles/card-menu.css";
+
+class CardMenu extends React.Component {
+    
+}
+export default CardMenu
--- src/components/home/sidebar-right.js
+++ src/components/home/sidebar-right.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "../../styles/home/sidebar-right.css";
-import AddFoodLogo from "../../assets/food-and-restaurant.png"
+// import AddFoodLogo from "../../assets/food-and-restaurant.png"
 
 
 class Sidecart extends React.Component {
     render() {
+        const { menus } = this.props
         return (
             <>
-                <div className="cart-items">
-                    <img src={AddFoodLogo} alt=""/>
-                        <div className="no-chart">
-                            <h4 className="font-weight-bold">Your cart is empty</h4>
-                            <p>Please add some items from the menu</p>
-                        </div>
-                </div>
+                <h3>done mbut</h3>
+                <ol>
+                    {menus.map((menu) => {
+                        return (
+                            <li key={menu.id}>
+                                <p>{menu.nama_produk}</p>
+                            </li>
+                        )
+                    })}
+                </ol>
             </>
         )
     }
 }
 export default Sidecart
+
